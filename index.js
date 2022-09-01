e3c633c19addcbeef921e6ed56ae4942e79b7037
Chris Drake <christopher.a.drake@gmail.com>
2022-09-01 16:26:49 -0400

    Updated messages and create/join flow

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const express = require("express");
 const ws = require("ws");
 const queryString = require("query-string");
 const { v4: uuidv4 } = require("uuid");
-const ADD_VOLUME_URL = "add volume url";
-const UPDATE = "update";
-const CREATE = "create";
-const JOIN = "join";
-const REMOVE_VOLUME_URL = "remove volume media";
-const ADD_MESH_URL = "add mesh url";
-const REMOVE_MESH_URL = "remove mesh media";
-const SET_4D_VOL_INDEX = "set 4d vol index";
-const UPDATE_IMAGE_OPTIONS = "update image options";
-const ACK = "ack";
-const UPDATE_CROSSHAIR_POS = "update crosshair pos";
-const CROSSHAIR_POS_UPDATED = "crosshair pos updated";
-const USER_JOINED = "user joined";
-const UPDATE_USER_STATE = "update user state";
-const USER_STATE_UPDATED = "user state updated";
-const SCENE_STATE_UPDATED = "scene state update";
+const MESSAGE = Object.freeze({
+  UPDATE: "update",
+  CREATE: "create",
+  JOIN: "join",
+  ADD_VOLUME_URL: "add volume url",
+  REMOVE_VOLUME_URL: "remove volume media",
+  ADD_MESH_URL: "add mesh url",
+  REMOVE_MESH_URL: "remove mesh media",
+  SET_4D_VOL_INDEX: "set 4d vol index",
+  UPDATE_IMAGE_OPTIONS: "update image options",
+  UPDATE_CROSSHAIR_POS: "update crosshair pos",
+  CROSSHAIR_POS_UPDATED: "crosshair pos updated",
+  USER_JOINED: "user joined",
+  UPDATE_SCENE_STATE: "update scene state",
+  UPDATE_USER_STATE: "update user state",
+  USER_STATE_UPDATED: "user state updated",
+  SCENE_STATE_UPDATED: "scene state updated",
+  ACK: "ack",
+  SESSION_CREATED: "session created",
+  SESSION_JOINED: "session joined",
+});
 
 let sessionOwnersMap = new Map();
 let sessionMap = new Map();
@@ -112,23 +117,42 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
     const parsedMessage = JSON.parse(message);
     let res = {
       message: "OK",
-      op: ACK,
+      op: MESSAGE.ACK,
     };
 
     switch (parsedMessage.op) {
-      case CREATE:
-        res.op = CREATE;
+      case MESSAGE.CREATE:
+        res.op = MESSAGE.SESSION_CREATED;
         // check if we already have the session
         if (sessionMap.has(session)) {
-          res.message = "duplicate session";
-          res.isError = true;
+          res.op = MESSAGE.SESSION_JOINED;
+          res.message = "session already exists";
+          res.isError = true;          
+          res["isController"] = parsedMessage.key === scene.key;          
+          res["url"] = getSessionUrl(connectionRequest, session);
+          res["userList"] = Array.from(userMap.values());
+          let userKey = assignUser(parsedMessage);
+          res["userKey"] = userKey;
+          let user = userMap.get(userKey);
+          res["userId"] = user.id;
+          res["userName"] = user.displayName;
+
+          // add user as controller
+          if(res["isController"]) {
+            sessionOwnersMap.get(session).push(user.id);
+          }
+
+          sendOtherClientsMessage(websocketConnection, {
+            op: MESSAGE.USER_JOINED,
+            user: userMap.get(res["userKey"]),
+          });
         } else {
           scene = {
             elevation: 0,
             azimuth: 0,
             zoom: 1.0,
             cliplane: [0, 0, 0, 0],
-            key: uuidv4(),
+            key: parsedMessage.key ? parsedMessage.key : uuidv4(),
           };
           sessionMap.set(session, scene);
           console.log("scene created for " + session);
@@ -139,7 +163,7 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
           res["userId"] = user.id;
           res["userKey"] = userKey;
           res["userName"] = user.displayName;
-
+          res["isController"] = true;
           // add this as a session owner
           sessionOwnersMap.set(session, [user.id]);
 
@@ -151,11 +175,11 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
                 connectionUserMap.get(websocketConnection)
             );
           }
-          // console.log('created session ' + session);
+          console.log('created session ' + session);
           // console.log('url: ' + res['url']);
         }
         break;
-      case UPDATE:
+      case MESSAGE.UPDATE_SCENE_STATE:
         // console.log('update message called');
         // only allow requests with session key to update
         if (scene.key === parsedMessage.key) {
@@ -165,7 +189,7 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
           scene.clipPlane = parsedMessage.clipPlane;
           // console.log('with correct key');
           sendOtherClientsMessage(websocketConnection, {
-            op: UPDATE,
+            op: MESSAGE.SCENE_STATE_UPDATED,
             message: "OK",
             azimuth: scene.azimuth,
             elevation: scene.elevation,
@@ -174,9 +198,9 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
           });
         }
         break;
-      case JOIN:
+      case MESSAGE.JOIN:
         if (scene) {
-          res.op = JOIN;
+          res.op = SESSION_JOINED;
           res["isController"] = parsedMessage.key === scene.key;          
           res["url"] = getSessionUrl(connectionRequest, session);
           res["userList"] = Array.from(userMap.values());
@@ -200,15 +224,15 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
             );
           }
           sendOtherClientsMessage(websocketConnection, {
-            op: USER_JOINED,
+            op: MESSAGE.USER_JOINED,
             user: userMap.get(res["userKey"]),
           });
         } else {
           console.log("scene for " + session + " not found");
         }
         break;
-      case UPDATE_IMAGE_OPTIONS:
-      case ADD_VOLUME_URL:
+      case MESSAGE.UPDATE_IMAGE_OPTIONS:
+      case MESSAGE.ADD_VOLUME_URL:
         if (scene.key === parsedMessage.key) {
           let msg = {
             op: parsedMessage.op,
@@ -217,7 +241,7 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
           sendOtherClientsMessage(websocketConnection, msg);
         }
         break;
-      case REMOVE_VOLUME_URL:
+      case MESSAGE.REMOVE_VOLUME_URL:
         if (scene.key === parsedMessage.key) {
           sendOtherClientsMessage(websocketConnection, {
             op: REMOVE_VOLUME_URL,
@@ -225,7 +249,7 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
           });
         }
         break;
-      case SET_4D_VOL_INDEX:
+      case MESSAGE.SET_4D_VOL_INDEX:
         if (scene.key === parsedMessage.key) {
           sendOtherClientsMessage(websocketConnection, {
             op: SET_4D_VOL_INDEX,
@@ -234,7 +258,7 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
           });
         }
         break;
-      case ADD_MESH_URL:
+      case MESSAGE.ADD_MESH_URL:
         if (scene.key === parsedMessage.key) {
           let msg = {
             op: parsedMessage.op,
@@ -243,15 +267,15 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
           sendOtherClientsMessage(websocketConnection, msg);
         }
         break;
-      case REMOVE_MESH_URL:
+      case MESSAGE.REMOVE_MESH_URL:
         if (scene.key === parsedMessage.key) {
           sendOtherClientsMessage(websocketConnection, {
-            op: REMOVE_MESH_URL,
+            op: MESSAGE.REMOVE_MESH_URL,
             url: parsedMessage.url,
           });
         }
         break;
-      case UPDATE_USER_STATE:
+      case MESSAGE.UPDATE_USER_STATE:
         if (userMap.has(parsedMessage.userKey)) {
           let user = userMap.get(parsedMessage.userKey);
           if (parsedMessage.id == user.id) {
@@ -263,17 +287,17 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
         }
         break;
 
-      case UPDATE_CROSSHAIR_POS:
+      case MESSAGE.UPDATE_CROSSHAIR_POS:
         if (userMap.has(parsedMessage.userKey)) {
           let user = userMap.get(parsedMessage.userKey);
           if (parsedMessage.id == user.id) {
-            console.log("updating crosshairs for " + user.displayName);
-            console.log(parsedMessage.crosshairPos);
+            // console.log("updating crosshairs for " + user.displayName);
+            // console.log(parsedMessage.crosshairPos);
 
             user.crosshairPos = parsedMessage.crosshairPos;
             userMap.set(parsedMessage.userKey, user);
             let msg = {
-              op: CROSSHAIR_POS_UPDATED,
+              op: CMESSAGE.ROSSHAIR_POS_UPDATED,
               id: user.id,
               isController: sessionOwnersMap.get(session).includes(user.id),
               crosshairPos: parsedMessage.crosshairPos,
@@ -285,7 +309,7 @@ wsServer.on("connection", (websocketConnection, connectionRequest) => {
         break;
 
       default:
-        res["op"] = SCENE_STATE_UPDATED;
+        res["op"] = MESSAGE.SCENE_STATE_UPDATED;
         res["azimuth"] = scene.azimuth;
         res["elevation"] = scene.elevation;
         res["zoom"] = scene.zoom;
